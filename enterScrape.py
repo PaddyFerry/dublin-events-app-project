34c31ef5b71e5abf6964dc93d7647978589c046e
baranad2 <dovydas.baranauskas2@mail.dcu.ie>
2017-11-03 19:41:37 +0000

    Added feature to show informatin/venue/price/phone

--- enterScrape.py
+++ enterScrape.py
@@ -8,31 +8,43 @@ class enterScrape(object):
 
 	def getLink(self):
 		linkList = []
-		soup = BeautifulSoup(self.openLink(),'html.parser')
+		soup = BeautifulSoup(self.openLink(),'html.parser') # open the link
 		linkDiv = soup.find_all('a',class_ = 'blcklink')
-		parentUrl = self.url.rsplit("/",3)
+		parentUrl = self.url.rsplit("/",3) #show the link with no extra pages
 		for i in linkDiv:
 			linkList.append(parentUrl[0]+ i.get('href'))
 		return linkList
 
 	def getInfo(self,url):
 		soup = BeautifulSoup(url,'html.parser')
-		linkDiv = soup.find_all('div',class_ = 'eventDetails')
-		for i in linkDiv:
-			print i.p
+		linkDiv = soup.find('div',class_ = 'listing maincol')
+		name = soup.find('div',class_ = 'eventDetails')
+		print name.find('h1').getText()
+		ps = linkDiv.find_all('p')
+		x = [x.getText().split('\n') for x in ps]
+		i = 0
+		while i < 6:
+			try:
+				print x[i][0]
+				i += 1
+			except:
+				break
+
+
 		return linkDiv
 
 	def openLink(self):
-		return urllib.urlopen(self.url).read()
+		return urllib.urlopen(self.url).read() #open the link and view as HTML
 
 
 def main():
 	url ='http://entertainment.ie/music/listings/'
 	scrp = enterScrape(url)
 	links = scrp.getLink()
-	url = urllib.quote(links[0].encode('utf8'), ':/')
-	testyLink = urllib.urlopen(url).read()
-	print scrp.getInfo(testyLink)
+	for i in links:
+		url = urllib.quote(i.encode('utf8'), ':/')
+		encodedLink = urllib.urlopen(url).read()
+		scrp.getInfo(encodedLink)
 
 
 if __name__ == '__main__':
