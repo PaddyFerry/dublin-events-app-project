98b537b0e4721ac957160552fed17dad3d41a442
baranad2 <dovydas.baranauskas2@mail.dcu.ie>
2017-11-15 20:34:45 +0000

    More generalised get link method

--- enterScrape.py
+++ enterScrape.py
@@ -6,13 +6,14 @@ class enterScrape(object):
 	def __init__(self,url=""):
 		self.url = url
 
-	def getLink(self):
+	def getLink(self,divID):
 		linkList = []
 		soup = BeautifulSoup(self.openLink(),'html.parser') # open the link
-		linkDiv = soup.find_all('a',class_ = 'blcklink')
-		parentUrl = self.url.rsplit("/",3) #show the link with no extra pages
+		linkDiv = soup.find_all('div',class_ = divID)
+		parentUrl = self.url.rsplit("/",3) #show the home link 
 		for i in linkDiv:
-			linkList.append(parentUrl[0]+ i.get('href'))
+			links = i.find('a')
+			print links['href'].split('.ie')
 		return linkList
 
 	def getInfo(self,url):
@@ -22,30 +23,36 @@ class enterScrape(object):
 		print name.find('h1').getText()
 		ps = linkDiv.find_all('p')
 		x = [x.getText().split('\n') for x in ps]
-		i = 0
+		# i = 0
 		while i < 6:
 			try:
 				print x[i][0]
 				i += 1
 			except:
 				break
-
-
 		return linkDiv
-
 	def openLink(self):
 		return urllib.urlopen(self.url).read() #open the link and view as HTML
 
 
 def main():
-	url ='http://entertainment.ie/music/listings/'
-	scrp = enterScrape(url)
-	links = scrp.getLink()
+	urls ='http://entertainment.ie/music/listings/'
+	scrp = enterScrape(urls)
+	links = scrp.getLink('listingDetails')
+	print links
 	for i in links:
 		url = urllib.quote(i.encode('utf8'), ':/')
 		encodedLink = urllib.urlopen(url).read()
 		scrp.getInfo(encodedLink)
 
+	# urls ='https://www.eventbrite.ie/d/ireland--dublin/music--events/?crt=regular&sort=date'
+	# scrp = enterScrape(urls)
+	# links = scrp.getLink('list-card-v2 l-mar-top-2 js-d-poster')
+	# print links
+	# # for i in links:
+	# # 	url = urllib.quote(i.encode('utf8'), ':/')
+	# # 	encodedLink = urllib.urlopen(url).read()
+	# # 	scrp.getInfo(encodedLink)
 
 if __name__ == '__main__':
 	main()
