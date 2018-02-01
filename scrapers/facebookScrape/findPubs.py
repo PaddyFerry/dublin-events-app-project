ce0f3ed73fe0508c1729b250baa2a8d87c2260b2
Dovydas Baranauskas <dovydas.baranauskas2@mail.dcu.ie>
2018-02-01 17:45:17 +0000

    The facebook scraper now gets all relevent info

--- scrapers/facebookScrape/findPubs.py
+++ scrapers/facebookScrape/findPubs.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib2,io,facebook
 
-
+# file = io.open("out.txt","a",encoding="utf8")
 # view = urllib2.urlopen("https://www.visitdublin.com/see-do/eat-drink-nightlife/bars-and-clubs?pa=0&pp=100&pg=2").read()
 # soup = BeautifulSoup(view,'lxml')
 # res = soup.find('ul',{"class":"no-bullet listing-results"})
@@ -11,15 +11,56 @@ import urllib2,io,facebook
 # 	file.write(desc.getText()+"\n")
 # file.close()
 
-graph = facebook.GraphAPI(access_token="EAANzGl7D69MBAEzqZB5n2oAFSxzf6YrY3bDgJoXSdmNNn73v4mtwCZCw6qQmuRY78n3QZAmBuyMRePFo4LJCdOE3mCHVcDOZB0otFKjFOZCO3cja9Km4ZAF2eciXlFIKr536h6gLwZBJJZBZCTdBlNKCBrAGEiVzxwonBJgA9bIrO9wZDZD", version="2.11")
+# graph = facebook.GraphAPI(access_token="EAANzGl7D69MBAMd3ooPuxbDH58EUQkI7e05ZA2z3fIby6JD9yOFi32jDRqKIGrQkUQW1dcm0s4FVYE03d7xtxWQKBnIGeRY2XNPcWrsjVWNZBfzXku46xkaViNTU2Hj2qxxak8KPjhXS9tt8ZByY3ZCHiICsc2Q5tPq1n7ZBW3bEHo9sYIx8E", version="2.11")
 
-file = io.open("out.txt","a",encoding="utf8")
-with open("out.txt") as f:
-	for line in f:
-		line = line.strip()
-		page_id = graph.search(type='place',q =line,limit= 1)
-		#try:
-		file.write((line+"-"+page_id['data'][0]['id']+"\n"))
-		#except:
-		print page_id
-file.close()
+# file = io.open("out.txt","a",encoding="utf8")
+# with open("out.txt") as f:
+# 	for line in f:
+# 		line = line.strip()
+# 		place_results = 0
+# 		page_results = 0
+# 		try:
+# 			place = graph.search(type='place', q=line, limit=1)
+# 			place_id = place["data"][0]["id"]
+# 			obj = graph.get_object(id=place_id,fields="location")
+# 			place_results = obj.get("location")
+# 			print("Trying place")
+# 			if place_results is not None:
+# 				if "Ireland" in place_results.values():
+# 					file.write((line+"-"+place['data'][0]['id']+"\n"))
+# 					print("Added")
+# 					print(place_results)
+
+# 				else:
+# 					place = graph.search(type='page', q=line, limit=1)
+# 					page_id = place["data"][0]["id"]
+# 					obj = graph.get_object(id=page_id,fields="location")
+# 					page_results = obj.get("location")
+# 					print("Trying page")
+# 					if page_results is not None:
+# 						if "Ireland" in page_results.values():
+# 							file.write((line+"-"+place['data'][0]['id']+"\n"))
+# 							print("Added")
+# 							print(page_results)
+
+# 					else:
+# 						print("Not in Ireland")
+
+
+# 		except IndexError:
+# 			try:
+# 				place = graph.search(type='page', q=line, limit=1)
+# 				place_id = place["data"][0]["id"]
+# 				obj = graph.get_object(id=place_id,fields="location")
+# 				page_results = obj.get("location")
+# 				print("Skip place trying page")
+# 				if page_results is not None:
+# 					if "Ireland" in page_results.values():
+# 						file.write((line+"-"+place['data'][0]['id']+"\n"))
+# 						print(page_results)
+# 						print("Added")
+
+# 			except IndexError:
+# 				print "No Info found"
+# 				continue
+# file.close()
