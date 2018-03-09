71356c021f55040499ce1508ea896b87391656cf
Patrick Ferry <patrick.ferry2@mail.dcu.ie>
2018-03-09 13:05:47 +0000

    Update scrape.py

--- code/scrapers/scrape.py
+++ code/scrapers/scrape.py
@@ -5,15 +5,7 @@ import threading
 import sys
 import runner
 
-# db = Database("test", "1234", "159.65.84.145", "app")
-# with db:
-#     for event in entertainment.get_info():
-#         try:
-#             db.check_add_event(event)
-#         except UnicodeEncodeError as err:
-#             print("Error : {}".format(err))
-#             print("ERROR WITH", event)
-#         print ("________________________________________")
+
 db = database.Database("test", "1234", "159.65.84.145", "app")
 if sys.argv[1].lower() == "entertainment":
     with db:
@@ -27,7 +19,3 @@ elif sys.argv[1].lower() == "facebook":
     with db:
         db.run(runner.run())
 
-
-# threading.Thread(target=db.run, args=(eventbrite.get_info(),)).start()
-# print("Lol")
-# threading.Thread(target=db.run, args=(entertainment.get_info(),)).start()
